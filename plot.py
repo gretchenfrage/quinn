45cf3c0846401d6573b5c5f24a3046d97efb2b78
Phoenix K <kahlo.phoenix@gmail.com>
2024-04-02 23:42:04 -0500

    science!

--- plot.py
+++ plot.py
@@ -1,38 +1,36 @@
 import matplotlib.pyplot as plt
 import json
+from collections import defaultdict
 
 def plot_results(filename):
-    inputs = []
-    latencies = [[] for _ in range(4)]  # Four lists to store each type of latency
+    latencies_by_input = defaultdict(lambda: defaultdict(list))  # input_size -> latency type -> values
 
     with open(filename, 'r') as file:
         for line in file:
             data = json.loads(line)
-            inputs.append(data[0])
-            for i, latency in enumerate(data[1:]):
-                latencies[i].append(latency)
+            input_size = data["input_size"]
+            for i, latency in enumerate(data["latencies"]):
+                latencies_by_input[input_size][i].append(latency)
 
-    fig, axs = plt.subplots(2, 1, figsize=(14, 16))  # Create two vertically stacked subplots
+    # Determine global min and max for y-axis range
+    all_latencies = [latency for latencies in latencies_by_input.values() for latency_list in latencies.values() for latency in latency_list]
+    y_min, y_max = min(all_latencies), max(all_latencies)
+
+    fig, axs = plt.subplots(2, 2, figsize=(12, 12))  # 2x2 grid
+    axs = axs.flatten()  # Flatten to iterate easily
     labels = ["1-RTT start latency", "0-RTT start latency", "1-RTT end latency", "0-RTT end latency"]
     colors = ['blue', 'red', 'green', 'purple']
-    
-    # Start latency subplot
-    axs[0].plot(inputs, latencies[0], marker='o', color=colors[0], label=labels[0])
-    axs[0].plot(inputs, latencies[1], marker='o', color=colors[1], label=labels[1])
-    axs[0].set_title('Start Latency')
-    axs[0].set_xlabel('Response Size')
-    axs[0].set_ylabel('Latency (ms)')
-    axs[0].legend()
-    axs[0].grid(True)
-    
-    # End latency subplot
-    axs[1].plot(inputs, latencies[2], marker='o', color=colors[2], label=labels[2])
-    axs[1].plot(inputs, latencies[3], marker='o', color=colors[3], label=labels[3])
-    axs[1].set_title('End Latency')
-    axs[1].set_xlabel('Response Size')
-    axs[1].set_ylabel('Latency (ms)')
-    axs[1].legend()
-    axs[1].grid(True)
+
+    for i, (label, color) in enumerate(zip(labels, colors)):
+        inputs = sorted(latencies_by_input.keys())
+        latencies = [latencies_by_input[input_size][i][0] for input_size in inputs if latencies_by_input[input_size][i]]
+        axs[i].plot(inputs, latencies, marker='o', color=color, label=label)
+        axs[i].set_title(label)
+        axs[i].set_xlabel('Response Size')
+        axs[i].set_ylabel('Latency (ms)')
+        axs[i].set_ylim([y_min, y_max])  # Set common y-axis range
+        axs[i].grid(True)
+        axs[i].legend()
 
     plt.tight_layout()
     plt.show()
